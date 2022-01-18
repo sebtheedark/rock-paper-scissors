d023fef5b29440182299bdea09afe86a237bd273
Kevin De La Torre <61712433+sebtheedark@users.noreply.github.com>
2022-01-17 16:20:52 -0800

    Play round function

--- scripts.js
+++ scripts.js
@@ -6,12 +6,41 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection > computerSelection)
-        return "Player wins," + playerSelection + '!'
-    
-    return playerSelection
+    // Rock 
+    if (playerSelection == 'rock') {
+        if (computerSelection == 'scissors') {
+            return 'You win! Rock beats Scissors!'
+        } else if (computerSelection == 'paper') {
+            return 'You lose! Paper beats rock!'
+        } else if (computerSelection == 'rock') {
+            return 'Its a tie!'
+        }
+
+    }
+
+    // Scissors
+    if (playerSelection == 'scissors') {
+        if (computerSelection == 'rock') {
+            return 'You lose! Rock beats scissors!'
+        } else if (computerSelection == 'paper') {
+            return 'You win! Scissors beats paper!'
+        } else if (computerSelection == 'scissors') {
+            return 'Its a tie!'
+        }
+    }
+
+    // Paper
+    if (playerSelection == 'paper') {
+        if (computerSelection == 'rock') {
+            return 'You win! Paper beats rock!'
+        } else if (computerSelection == 'scissors') {
+            return 'You lose! Scissors beats paper!'
+        } else if (computerSelection == 'paper') {
+            return 'Its a tie!'
+        }
+    }
 }
-const playerSelection = 'rock'
+const playerSelection = 'paper'
 const computerSelection = computerPlay();
 
 console.log(playRound(playerSelection, computerSelection));
